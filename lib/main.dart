a4820b225307430c5714b4ad6fc4f9722047d6a7
Maeve Kelly <89049979+mave1@users.noreply.github.com>
2024-04-30 20:29:22 +1000

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_map_marker_popup/flutter_map_marker_popup.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -24,13 +27,31 @@ class _MyAppState extends State<MyApp>{
             Flexible(   
               child: FlutterMap(
                 options: const MapOptions(
-                  initialCenter: LatLng(-41.45451960, 145.97066470),
+                  initialCenter: LatLng(-42.87936, 147.32941),
                   initialZoom: 7,
                 ),
                 children: [
                   TileLayer(
                     urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                     userAgentPackageName: 'com.example.app',
+                  ),
+                  PopupMarkerLayer(
+                    options: PopupMarkerLayerOptions( 
+                      markers: [
+                        const Marker(
+                          point: LatLng(-40.87936, 147.32941), 
+                          child: Icon(
+                            Icons.location_pin,
+                            size: 40,
+                            color: Colors.pink,
+                          ))
+                      ],
+                      popupDisplayOptions: PopupDisplayOptions(
+                        builder: (BuildContext context, Marker marker) => Container(
+                          color: Colors.white,
+                          child: Text(informationPopup(marker)),
+                        ),
+                    ))
                   ),
                   RichAttributionWidget(
                     attributions: [
@@ -49,3 +70,9 @@ class _MyAppState extends State<MyApp>{
     );
   }
 }
+
+informationPopup(Marker marker) {
+  return 'popupB';
+}
+
+
