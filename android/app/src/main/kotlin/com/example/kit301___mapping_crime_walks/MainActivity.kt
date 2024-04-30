eee8514ae191371b7de04aa91e7f63757a7ac387
Tmfos <rowan_mcloughlin@hotmail.com>
2024-04-30 17:43:09 +1000

    Markers on the map added

---
+++ android/app/src/main/kotlin/com/example/kit301___mapping_crime_walks/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kit301___mapping_crime_walks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: MyApp(),
     ));
 }
 
 class MyApp extends StatefulWidget{
+  const MyApp({super.key});
+
   @override
   _MyAppState createState() => _MyAppState();
 }
@@ -17,6 +19,42 @@ class MyApp extends StatefulWidget{
 class _MyAppState extends State<MyApp>{
   @override
   Widget build(BuildContext context) {
+    var markerLocations = <Marker>[]; // marker list variable used to add markers onto map
+
+    // list of locations within the app
+    markerLocations = [
+      Marker(
+        point: const LatLng(-42.90395, 147.325439), 
+        width: 40,
+        height: 40,
+        child: GestureDetector(
+          onTap: () {
+            // TODO: open walk information here
+          },
+          child: const Icon(
+          Icons.location_pin,
+          size: 40,
+          color: Colors.red,
+          ),
+        ),
+      ),
+      Marker(
+        point: const LatLng(-42.879601, 147.329874), 
+        width: 40,
+        height: 40,
+        child: GestureDetector(
+          onTap: () {
+            // TODO: open walk information here
+          },
+          child: const Icon(
+          Icons.location_pin,
+          size: 40,
+          color: Colors.red,
+          ),
+        ),
+      ),
+    ];
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -28,18 +66,9 @@ class _MyAppState extends State<MyApp>{
                   initialZoom: 7,
                 ),
                 children: [
-                  TileLayer(
-                    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    userAgentPackageName: 'com.example.app',
-                  ),
-                  RichAttributionWidget(
-                    attributions: [
-                      TextSourceAttribution(
-                        'OpenStreetMap contributors',
-                        onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
-                      ),
-                    ],
-                  ),
+                  openStreetMapTileLayer, //input map
+                  copyrightNotice, // input copyright
+                  MarkerLayer(markers: markerLocations), //input markers
                 ],
               ),
             ),
@@ -49,3 +78,19 @@ class _MyAppState extends State<MyApp>{
     );
   }
 }
+
+// retrieve openstreetmap map
+TileLayer get openStreetMapTileLayer => TileLayer(
+    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    userAgentPackageName: 'com.example.app',
+);
+
+// retrieve copyright notice
+RichAttributionWidget get copyrightNotice => RichAttributionWidget(
+  attributions: [
+    TextSourceAttribution(
+      'OpenStreetMap contributors',
+      onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+    ),
+  ],
+);
