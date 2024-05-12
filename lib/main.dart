b97412350de5cf998c8952f286e85a899e2b9c8c
TasmanianNoob <charlierc2003@gmail.com>
2024-05-12 19:08:26 +1000

    please show up on github

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:crimewalksapp/crime_walk.dart';
 import 'package:crimewalksapp/filtered_list.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:url_launcher/url_launcher.dart';
